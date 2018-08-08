c173e5917bf6521b5dd1cc7b9468e0c006869fa8
wycer <734849165@qq.com>
2018-08-08 11:25:19 +0800

    后缀数组裸题

---
+++ Lydsy/P1031 [JSOI2007]字符加密Cipher/P1031.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+const int N = 2e5 + 10;
+char str[N]; int n, len, sa[2][N], rk[2][N], v[N], a[N];
+void Init()
+{
+    scanf("%s", str + 1);
+    len = n = strlen(str + 1);
+    for (int i = 1; i <= n; ++i)
+        a[i + n] = a[i] = int(str[i + n] = str[i]);
+    n <<= 1;
+}
+
+void CalSA(int k, int *sa, int *rk, int *SA, int *RK)
+{
+    for (int i = 1; i <= n; ++i)
+        v[rk[sa[i]]] = i;
+    for (int i = n; i >= 1; --i)
+        if (sa[i] > k)
+            SA[v[rk[sa[i] - k]]--] = sa[i] - k;
+    for (int i = n - k + 1; i <= n; ++i)
+        SA[v[rk[i]]--] = i;
+    for (int i = 1; i <= n; ++i)
+        RK[SA[i]] = RK[SA[i - 1]] + (rk[SA[i - 1]] != rk[SA[i]] || rk[SA[i - 1] + k] != rk[SA[i] + k]);
+}
+int GetSA()
+{
+    int p = 0, q = 1;
+    memset(v, 0, sizeof v);
+    for (int i = 1; i <= n; ++i)
+        v[a[i]]++;
+    for (int i = 1; i <= 256; ++i)
+        v[i] += v[i - 1];
+    for (int i = 1; i <= n; ++i)
+        sa[p][v[a[i]]--] = i;
+    for (int i = 1; i <= n; ++i)
+        rk[p][sa[p][i]] = rk[p][sa[p][i - 1]] + (a[sa[p][i]] != a[sa[p][i - 1]]);
+    for (int k = 1; k < n; swap(p, q), k <<= 1)
+        CalSA(k, sa[p], rk[p], sa[q], rk[q]);
+    return p;
+}
+
+void Work()
+{
+    int res = GetSA();
+    for (int i = 1; i <= n; ++i) 
+        if (sa[res][i] <= len)
+            printf("%c", str[sa[res][i] + len - 1]);  
+} 
+
+int main()
+{
+    Init();
+    Work();
+    return 0;
+}
