d0774e9e7bbb9636bd9e0e18714d15f747a8435a
Wycer <734849165@qq.com>
2018-08-21 11:54:19 +0800

    更新向量基础模板

--- Computation-geometry/base.cpp
+++ Computation-geometry/base.cpp
@@ -3,41 +3,63 @@
 const double pi = acos(-1.0);
 const double eps = 1e-8;
 
-struct Vector
-{
-    double x, y;
-};
-
-Vector operator+(const Vector &a, const Vector &b)
-{
-    return (Vector){a.x + b.x, a.y + b.y};
-}
-Vector operator-(const Vector &a, const Vector &b)
-{
-    return (Vector){a.x - b.x, a.y - b.y};
-}
-Vector operator*(const Vector &a, const double &k)
-{
-    return (Vector){a.x * k, a.y * k};
-}
-Vector operator/(const Vector &a, const double &k)
-{
-    return (Vector){a.x / k, a.y / k};
-}
-double operator*(const Vector &a, const Vector &b)
-{
-    return a.x * b.x + a.y * b.y;
-}
-double operator^(const Vector &a, const Vector &b)
-{
-    return a.x * b.y - a.y * b.x;
-}
 int sign(double x)
 {
     if (fabs(x) < eps)
         return 0;
     return x > 0 ? 1 : -1;
 }
+
+struct Vector
+{
+    double x, y;
+    void input() { scanf("%lf%lf", &x, &y); }
+    void output() { printf("%.2lf %.2lf\n", x, y); }
+    Vector (double _x, double _y) { 
+        x = _x; y = _y; 
+    }
+    bool operator == (const Vector &b) const {
+        return sign(x - b.x) == 0 && sign(y - b.y) == 0;
+    }
+    Vector operator + (const Vector &b) const {
+        return Vector(x + b.x, y + b.y);
+    }
+    Vector operator - (const Vector &b) const {
+        return Vector(x - b.x, y - b.y);
+    }
+    Vector operator * (const double &k) const {
+        return Vector(x * k, y * k);
+    }
+    Vector operator / (const double &k) const {
+        return Vector(x / k, y / k);
+    }
+    double operator * (const Vector &b) const {
+        return x * b.x + y * b.y;
+    }
+    double operator ^ (const Vector &b) const {
+        return x * b.y - y * b.x;
+    }
+    double length() {
+        return hypot(x, y);
+    }
+    double lengths() {
+        return x * x + y * y;
+    }
+    double distance(const Vector &b) const {
+        return hypot(x - b.x, y - b.y);
+    }
+    Vector rotccw() {
+        return Vector(-y, x);
+    }
+    Vector rotcw() {
+        return Vector(y, -x);
+    }
+    Vector rot(double rad, const Vector &b) const {
+        Vector v = (*this) - b, t = Vector(sin(rad), cos(rad));
+        return b + Vector(v ^ t, v * t);
+    }
+};
+
 inline double Area2(Vector &a, Vector &b, Vector &c)
 {
     Vector t1 = b - a;
@@ -53,7 +75,7 @@ inline double dot(Vector &a, Vector &b, Vector &c)
 inline double dist(Vector &a, Vector &b)
 {
     Vector t = b - a;
-    return sqrt(t * t);
+    return t.length();
 }
 
 int main()
