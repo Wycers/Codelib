3f52b586ddf6a173d10600212f3263dfce12ccd3
wycer <734849165@qq.com>
2018-09-06 15:26:25 +0800

    更新板子

--- Template/Computation-geometry/halfplane/halfplane.cpp
+++ Template/Computation-geometry/halfplane/halfplane.cpp
@@ -1,63 +1,49 @@
 #include "../polygon/polygon.cpp"
 
-const int LN = 10;
 struct halfplane
 {
     int n;
-    Line hp[LN];
-    Vector p[LN];
-    int que[LN];
+    Line hp[N], *q[N];
+    Vector p[N];
     int st, ed;
     void push(Line tmp)
     {
         hp[n++] = tmp;
     }
-    void unique()
+    void halfplaneinsert()
     {
-        int m = 1;
+        sort(hp, hp + n);
+        q[st = ed = 0] = &hp[0];
         for (int i = 1; i < n; i++)
         {
-            if (sign(hp[i].ag - hp[i - 1].ag) != 0)
-                hp[m++] = hp[i];
-            else if (sign((hp[m - 1].e - hp[m - 1].s) ^ (hp[i].s - hp[m - 1].s)) > 0)
-                hp[m - 1] = hp[i];
+            while (st < ed && hp[i].prelation(p[ed - 1]) != 1)
+                --ed;       //Remove the top of the half plane
+            while (st < ed && hp[i].prelation(p[st]) != 1)
+                ++st;      //Remove the bottom half plane
+            q[++ed] = &hp[i]; //The half plane current if the double ended queue at the top.
+            if (sign(q[ed]->v ^ q[ed - 1]->v) == 0)
+            { //For the polar angle the same, selective retention of a.
+                --ed;
+                if ((*q[ed]).prelation(hp[i].s) == 1)
+                    q[ed] = &hp[i];
+            }
+            if (st < ed)
+                p[ed - 1] = (*q[ed - 1]).crosspoint(*q[ed]); //Calculation of the top of the queue half plane intersection.
         }
-        n = m;
+        while (st < ed && (*q[st]).prelation(p[ed - 1]) != 1)
+            --ed; //Remove the top of the queue of useless half plane.
+        if (ed - st <= 1)
+            return;
+        p[ed] = (*q[ed]).crosspoint(*q[st]); //The top of the queue and the first intersection calculation.
     }
-    bool halfplaneinsert()
+    bool getconvex(polygon &res)
     {
-        for (int i = 0; i < n; i++)
-            hp[i].calangle();
-        sort(hp, hp + n);
-        unique();
-        que[st = 0] = 0;
-        que[ed = 1] = 1;
-        p[1] = hp[0].crosspoint(hp[1]);
-        for (int i = 2; i < n; i++)
-        {
-            while (st < ed && sign((hp[i].e - hp[i].s) ^ (p[ed] - hp[i].s)) < 0)
-                ed--;
-            while (st < ed && sign((hp[i].e - hp[i].s) ^ (p[st + 1] - hp[i].s)) < 0)
-                st++;
-            que[++ed] = i;
-            if (hp[i].parallel(hp[que[ed - 1]]))
-                return false;
-            p[ed] = hp[i].crosspoint(hp[que[ed - 1]]);
-        }
-        while (st < ed && sign((hp[que[st]].e - hp[que[st]].s) ^ (p[ed] - hp[que[st]].s)) < 0)
-            ed--;
-        while (st < ed && sign((hp[que[ed]].e - hp[que[ed]].s) ^ (p[st + 1] - hp[que[ed]].s)) < 0)
-            st++;
-        if (st + 1 >= ed)
-            return false;
+        res.n = ed - st + 1;
+        if (ed - st <= 1)
+            return false;                                   //Half plane degradation
+        for (int i = st; i <= ed; i++)
+            res.p[i - st] = p[i]; //The queue of point.
         return true;
-    }
-    void getconvex(polygon &con)
-    {
-        p[st] = hp[que[st]].crosspoint(hp[que[ed]]);
-        con.n = ed - st + 1;
-        for (int j = st, i = 0; j <= ed; i++, j++)
-            con.p[i] = p[j];
     }
 };
 
