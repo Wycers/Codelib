5c405861083c0d61f9b67194ae8a4e4701b5d937
Wycer <734849165@qq.com>
2018-09-06 13:49:53 +0800

    不懂怎么回事就过了

--- Practice/HDU-2297.cpp
+++ Practice/HDU-2297.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
 const double pi = acos(-1.0);
 const double eps = 1e-8;
@@ -68,12 +69,17 @@ struct Vector
 };
 struct Line 
 {
-    Vector s, e;
+    Vector s, e, v;
     double ag;
     Line () {}
+    void init() {
+        v = e - s;
+        ag = atan2(v.y, v.x);
+    }
     Line (Vector _s, Vector _e) {
         s = _s;
         e = _e;
+        init();
     }
     bool operator == (const Line &b) const {
         return (s == b.s) && (e == b.e);
@@ -89,6 +95,8 @@ struct Line
             e = s + Vector(0, 1);
         else
             e = s + Vector(1, tan(rad));
+        init();
+        ag = atan2(v.y, v.x);
     }
     // 一般式确定直线
     Line (double a, double b, double c) {
@@ -96,14 +104,15 @@ struct Line
             s = Vector(0, -c / b);
             e = Vector(1, -c / b);
         }
-        if (sign(b) == 0) {
+        else if (sign(b) == 0) {
             s = Vector(-c / a, 0);
             e = Vector(-c / a, 1);
-        }
+        } 
         else {
             s = Vector(0, -c / b);
             e = Vector(1, (-c - a) / b);
         }
+        init();
     }
     void input() {
         s.input();
@@ -112,72 +121,19 @@ struct Line
     double length() {
         return s.distance(e);
     }
-    // 计算倾斜角
-    double angle() {
-        Vector t = e - s;
-        double k = atan2(t.x, t.y);
-        if (sign(k) == -1)
-            k += pi;
-        if (sign(k - pi) == 0)
-            k -= pi;
-        return k;
-    }
-    double calangle()
-    {
-        ag = atan2(e.y - s.y, e.x - s.x);
-    }
-    // 某点和该直线的关系，1在左，0在上，-1在右
-    int prelation (const Vector &p) {
-        return sign((e - s) ^ (p - s));
-    }
-    // 线与线是否平行
     bool parallel(const Line &b) {
-        return sign((e - s) ^ (b.e - b.s)) == 0;
+        return sign(v ^ b.v) == 0;
     }
-    // 某线和该直线的关系，0 - 平行， 1 - 重合， 2 - 相交
-    int linecrossline(Line v) {
-        if ((*this).parallel(v))
-            return v.prelation(s) == 0;
-        return 2;
-    }
-    // 点在线段是否在线段上
-    bool cross(const Vector &p) {
-        return prelation(p) == 0 && sign((p - s) ^ (p - e)) <= 0; 
+    bool onleft(const Vector &b) {
+        return sign(v ^ (b - s)) > 0;
     }
     // 求两直线的交点，要保证两直线不平行或重合
     Vector crosspoint(const Line &b) {
-        Vector v1 = e - s;
-        Vector v2 = b.e - b.s;
-        Vector t = s - b.s;
-        double tmp = (v2 ^ t) / (v1 ^ v2);
-        return s + v1 * tmp;
+        Vector u = s - b.s;
+        double t = (b.v ^ u) / (v ^ b.v);
+        return s + v * t;
     }
 };
-struct polygon 
-{
-    int n;
-    Vector p[N];
-    Line l[N];
-    void input(int _n) {
-        n = _n;
-        for (int i = 0; i < n; ++i)
-            p[i].input();
-    }
-    void add(Vector q) {
-        p[n++] = q;
-    }
-    void generate() {
-        for (int i = 0; i < n; ++i)
-            l[i] = Line(p[i], p[(i + 1) % n]);
-    }
-    // 求该多边形面积
-    double area() {
-        double res = 0;
-        for (int i = 0; i < n; i++)
-            res += (p[i] ^ p[(i + 1) % n]);
-        return fabs(res) / 2;
-    }
-} ans;
 struct halfplane
 {
     int n;
@@ -191,54 +147,31 @@ struct halfplane
         // tmp.s.output();
         // tmp.e.output();
     }
-    void unique()
+    int halfplaneinsert()
     {
-        int m = 1;
+        sort(hp, hp + n);
+	    vector<Line> q(n);
+        int first, last;
+        q[first = last = 0] = hp[0];
         for (int i = 1; i < n; i++)
         {
-            if (sign(hp[i].ag - hp[i - 1].ag) != 0)
-                hp[m++] = hp[i];
-            else if (sign((hp[m - 1].e - hp[m - 1].s) ^ (hp[i].s - hp[m - 1].s)) > 0)
-                hp[m - 1] = hp[i];
+            while (first < last && !hp[i].onleft(p[last - 1]))
+                last--;       //Remove the top of the half plane
+            while (first < last && !hp[i].onleft(p[first]))
+                first++;      //Remove the bottom half plane
+            q[++last] = hp[i]; //The half plane current if the double ended queue at the top.
+            if (sign(q[last].v ^ q[last - 1].v) == 0)
+            { //For the polar angle the same, selective retention of a.
+                last--;
+                if (q[last].onleft(hp[i].s))
+                    q[last] = hp[i];
+            }
+            if (first < last)
+                p[last - 1] = q[last-1].crosspoint(q[last]); //Calculation of the top of the queue half plane intersection.
         }
-        n = m;
-    }
-    bool halfplaneinsert()
-    {
-        for (int i = 0; i < n; i++)
-            hp[i].calangle();
-        sort(hp, hp + n);
-        unique();
-        que[st = 0] = 0;
-        que[ed = 1] = 1;
-        p[1] = hp[0].crosspoint(hp[1]);
-        for (int i = 2; i < n; i++)
-        {
-            while (st < ed && sign((hp[i].e - hp[i].s) ^ (p[ed] - hp[i].s)) < 0)
-                ed--;
-            while (st < ed && sign((hp[i].e - hp[i].s) ^ (p[st + 1] - hp[i].s)) < 0)
-                st++;
-            que[++ed] = i;
-            if (hp[i].parallel(hp[que[ed - 1]]))
-                return false;
-            p[ed] = hp[i].crosspoint(hp[que[ed - 1]]);
-        }
-        while (st < ed && sign((hp[que[st]].e - hp[que[st]].s) ^ (p[ed] - hp[que[st]].s)) < 0)
-            ed--;
-        while (st < ed && sign((hp[que[ed]].e - hp[que[ed]].s) ^ (p[st + 1] - hp[que[ed]].s)) < 0)
-            st++;
-        if (st + 1 >= ed)
-            return false;
-        return true;
-    }
-
-    //需要调用halfplaneinsert。
-    void getconvex(polygon &con)
-    {
-        p[st] = hp[que[st]].crosspoint(hp[que[ed]]);
-        con.n = ed - st + 1;
-        for (int j = st, i = 0; j <= ed; i++, j++)
-            con.p[i] = p[j];
+        while (first < last && !q[first].onleft(p[last - 1]))
+            last--; //Remove the top of the queue of useless half plane.
+        return last - first + 1;
     }
 } hp;
 
@@ -247,22 +180,19 @@ void solve()
 {
     scanf("%d", &n);
     hp.n = 0;
-    hp.push(Line(Vector(1e12, 1e12), Vector(-1, 1e12)));
-    hp.push(Line(Vector(0, 1e12), Vector(0, -1)));
+    hp.push(Line(Vector(1e10, 1e10), Vector(-1, 1e10)));
+    hp.push(Line(Vector(0, 1e10), Vector(0, -1)));
     for (int i = 0; i < n; ++i)
     {
         double b, k;
         scanf("%lf%lf", &b, &k);
-        hp.push(Line(k, -1, b));
+        hp.push(Line(Vector(0, b), Vector(1, b + k)));
     }
-    hp.halfplaneinsert();
-    hp.getconvex(ans);
-    // for (int i = 0; i < ans.n; ++i)
-    //     ans.p[i].output();
-    printf("%d\n", ans.n - 2);
+    printf("%d\n", hp.halfplaneinsert() - 2);
 }
 int main()
 {
+    freopen("test.in", "r", stdin);
     int T; scanf("%d", &T);
     while (T--)
         solve();
