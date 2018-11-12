e0b7e4c7140b84fce92116c4e145f271689e043d
wycer <734849165@qq.com>
2018-11-12 12:09:16 +0800

    lab6 E

--- DSAA/Lab 6/E.cpp
+++ DSAA/Lab 6/E.cpp
@@ -1,15 +1,26 @@
 #include <cstdio>
+#include <cstring>
 #include <algorithm>
 using namespace std;
 const int N = 1e5 + 10;
-int h[N], top;
+int cnt[1 << 20], enter[1 << 10], leave[1 << 20];
+struct data
+{
+    int type, cnt, ti;
+} h[N];
+bool operator < (const data &a, const data &b) {
+    if (a.cnt == b.cnt)
+        return a.ti > b.ti;
+    return a.cnt < b.cnt;
+}
+int n, m, top, g;
 void fix(int pos)
 {
     if (pos == 1)
         return;
     while (pos != 1)
     {
-        if (h[pos] < h[pos >> 1])
+        if (h[pos >> 1] < h[pos])
             swap(h[pos], h[pos >> 1]);
         else
             break;
@@ -18,7 +29,7 @@ void fix(int pos)
 }
 void add(int x)
 {
-    h[++top] = x;
+    h[++top] = (data){x, ++cnt[x], enter[x]};
     fix(top);
 }
 void del()
@@ -29,9 +40,9 @@ void del()
     while ((pos << 1) <= top)
     {
         int t = pos;
-        if (h[t] > h[pos << 1])
+        if (h[t] < h[pos << 1])
             t = pos << 1;
-        if ((pos << 1 | 1) <= top && h[t] > h[pos << 1 | 1])
+        if ((pos << 1 | 1) <= top && h[t] < h[pos << 1 | 1])
             t = pos << 1 | 1;
         if (t == pos)
             break;
@@ -41,26 +52,33 @@ void del()
 } 
 void solve()
 {
-    scanf("%d", &top);
-    for (int i = 1; i <= top; ++i)
+    scanf("%d%d", &m, &n);
+    top = 0; g = 0;
+    int x, ans = 0;
+    memset(cnt, 0, sizeof cnt);
+    memset(enter, 0, sizeof enter);
+    memset(leave, 0, sizeof leave);
+    for (int i = 1; i <= n; ++i)
     {
-        scanf("%d", h + i);
-        fix(i);
-    }
-    int q, opt, x; scanf("%d", &q);
-    while (q--)
-    {
-        scanf("%d", &opt);
-        if (opt == 1)
-        {
-            scanf("%d", &x);
-            add(x);
-        }
-        else if (opt == 2)
+        scanf("%d", &x);
+        while (leave[h[1].type] > h[1].ti)
             del();
-        else 
-            printf("%d\n", h[1]);
+        if (cnt[x] == 0 && g == m)
+        {
+            ++ans;
+            cnt[h[1].type] = 0; 
+            --g; 
+            leave[h[1].type] = i;
+            del();
+        }
+        if (cnt[x] == 0)
+        {
+            enter[x] = i;
+            ++g;
+        }
+        add(x);
     }
+    printf("%d\n", ans);
 }
 int main()
 {
