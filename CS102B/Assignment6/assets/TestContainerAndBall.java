08e5734ffd379307fef6c321460813f8a06bc35e
Wycer <734849165@qq.com>
2018-12-17 19:01:42 +0800

    测试第三题

--- CS102B/Assignment6/assets/TestContainerAndBall.java
+++ CS102B/Assignment6/assets/TestContainerAndBall.java
@@ -39,21 +39,24 @@ public class TestContainerAndBall {
 
 		return res;
 	}
-	public static int checkBallOutput(String ball, double x, double y, double vx, double vy) {
+	public static int checkBallOutput(String ball, double x, double y, double spead, double direction) {
 		String pattern = "(?i).*Ball\\s*at\\s*\\(\\s*((-?\\d+)(\\.\\d+)?)\\s*,\\s*((-?\\d+)(\\.\\d+)?)\\s*\\)\\s*of\\s*velocity\\s*\\(\\s*((-?\\d+)(\\.\\d+)?)\\s*,\\s*((-?\\d+)(\\.\\d+)?)\\s*\\)\\s*";
 		Pattern r = Pattern.compile(pattern);
 
+		double vx =  spead * Math.cos(direction / 180.0 * Math.PI);
+		double vy = -spead * Math.sin(direction / 180.0 * Math.PI);
+
 		Matcher m = r.matcher(ball);
 		if (m.find()) {
-			const double eps = 1e-8;
+			double eps = 1e-2;
 			try {
 				if (Math.abs(Double.parseDouble(m.group(1)) - x) > eps)
 					return 1;
-				if (Math.abs(Double.parseDouble(m.group(3)) - y) > eps)
+				if (Math.abs(Double.parseDouble(m.group(4)) - y) > eps)
 					return 1;
-				if (Math.abs(Double.parseDouble(m.group(5)) - vx) > eps)
+				if (Math.abs(Double.parseDouble(m.group(7)) - vx) > eps)
 					return 1;
-				if (Math.abs(Double.parseDouble(m.group(7)) - vy) > eps)
+				if (Math.abs(Double.parseDouble(m.group(10)) - vy) > eps)
 					return 1;
 			} catch (Exception e) {
 				return 1;
@@ -62,32 +65,51 @@ public class TestContainerAndBall {
 		}
 		return 1;
 	}
+	public static int checkBall() {
+		int res = 0;
+		Ball ball1 = new Ball(50, 50, 5, 70, 45);
+		res += checkBallOutput(ball1.toString(), 50, 50, 70, 45);
+
+		Ball ball2 = new Ball(10, 15, 5, 5, -60);
+		res += checkBallOutput(ball2.toString(), 10, 15, 5, -60);
+		
+		return res;
+	}
+
+	public static int checkMove(int sx, int sy, int tx, int ty, int r, int x, int y, int spead, int direction) {
+		int res = 0;
+		Container box = new Container(sx, sy, tx, ty);
+		Ball ball = new Ball((float)x, (float)y, r, spead, direction);
+		double vx =  spead * Math.cos(direction / 180.0 * Math.PI);
+		double vy = -spead * Math.sin(direction / 180.0 * Math.PI);
+		
+		tx = sx + tx - 1;
+		ty = sy + ty - 1;
+		while (true) {
+			ball.move();
+			x += vx; 
+			y += vy;
+			boolean collided = false;
+			collided |= x <= (sx + r);
+			collided |= x >= (tx - r);
+			collided |= y <= (sy + r);
+			collided |= y >= (ty - r);
+			if (box.collidesWith(ball) != collided) {
+				return 3;
+			}
+			if (collided)
+				return 0;
+		}
+	}
 	public static void main(String[] args) {
 		int score = 100;
-		score -= 5 * checkBox();
+		score -= 10 * checkBox();
+		score -= 10 * checkBall();
 
+		score -= 6 * checkMove(0, 0, 100, 100, 5, 50, 50, 70, 45);
+		score -= 6 * checkMove(0, 0, 80, 80, 5, 0, 0, 10, 45);
+		score -= 6 * checkMove(0, 0, 50, 50, 5, 25, 25, 5, 180);
+		score -= 6 * checkMove(0, 0, 75, 10, 2, 34, 5, 20, 73);
 		System.out.println(score);
-		// System.out.println(box);
-		// System.out.println("Please input the speed of the ball");
-		// int speed = scanner.nextInt();
-		// int direction;
-		// System.out.println("Please input the direction of the ball");
-		// while ((direction = scanner.nextInt()) <= -180 && direction > 180) {
-		// 	System.out.println("Invalide direction. Please input the direction of the ball");
-		// }
-		// Ball ball = new Ball(50, 50, 5, speed, direction);
-		// System.out.println(ball);
-		// System.out.println("The ball is moving now.");
-		// while (true) {
-		// 	ball.move();
-		// 	if (box.collidesWith(ball)) {
-		// 		System.out.println(ball);
-		// 		System.out.print("The Ball will be collided with the container. ");
-		// 		System.out.println();
-		// 		break;
-		// 	} else {
-		// 		System.out.println(ball);
-		// 	}
-		// }
 	}
 }
