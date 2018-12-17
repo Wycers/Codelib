4986bb3bb330ba6b15a97aa9415263be1d1a3b21
Wycer <734849165@qq.com>
2018-12-17 19:01:56 +0800

    合并

--- CS102B/Assignment6/Grader.py
+++ CS102B/Assignment6/Grader.py
@@ -1,67 +1,27 @@
 import subprocess, shutil
 from os import path, listdir, remove
-def testDateUtil():
-    filelist = ['DateUtil.java']
-    for file in filelist:
-        if not path.exists(path.join('.', 'user', file)):
-            print('File [ %s ] not found while testing DateUtil' % file)
-            return 0
-        shutil.copyfile(path.join('.', 'user', file), path.join('.', file))
-    shutil.copyfile(path.join('.', 'assets', 'TestDateUtil.java'), path.join('.', 'TestDateUtil.java'))
-    code, output = subprocess.getstatusoutput('javac %s' % path.join('.', 'TestDateUtil.java'))
-    if code is not 0: 
-        print('test file can not be compiled')
-        print(output)
-        return 0
-    for file in filelist:
-        code, output = subprocess.getstatusoutput('javac %s' % path.join('.', file))
-        if code is not 0:
-            print('user file can not be compiled')
-            return 0
-    code, output = subprocess.getstatusoutput('java TestDateUtil')
-    return int(output)
 
-def testPointAndLine():
-    filelist = ['Point.java', 'Line.java']
+def test(name, filelist):
     for file in filelist:
         if not path.exists(path.join('.', 'user', file)):
-            print('File [ %s ] not found while testing PointAndLine' % file)
+            print('[%s] File [ %s ] not found.' % (name, file))
             return 0
         shutil.copyfile(path.join('.', 'user', file), path.join('.', file))
-    shutil.copyfile(path.join('.', 'assets', 'TestPointAndLine.java'), path.join('.', 'TestPointAndLine.java'))
-    code, output = subprocess.getstatusoutput('javac %s' % path.join('.', 'TestPointAndLine.java'))
+    shutil.copyfile(path.join('.', 'assets', '%s.java' % name), path.join('.', '%s.java' % name))
+    code, output = subprocess.getstatusoutput('javac %s' % path.join('.', '%s.java' % name))
     if code is not 0: 
-        print('test file can not be compiled')
-        print(output)
+        print('[%s] test file can not be compiled' % name)
         return 0
     for file in filelist:
         code, output = subprocess.getstatusoutput('javac %s' % path.join('.', file))
         if code is not 0:
-            print('user file can not be compiled')
+            print('[%s] user file can not be compiled' % name)
             return 0
-    code, output = subprocess.getstatusoutput('java TestPointAndLine')
-    return int(output)
-
-def testContainerAndBall():
-    filelist = ['Container.java', 'Ball.java']
-    for file in filelist:
-        if not path.exists(path.join('.', 'user', file)):
-            print('File [ %s ] not found while testing ContainerAndBall' % file)
-            return 0
-        shutil.copyfile(path.join('.', 'user', file), path.join('.', file))
-    shutil.copyfile(path.join('.', 'assets', 'TestContainerAndBall.java'), path.join('.', 'TestContainerAndBall.java'))
-    code, output = subprocess.getstatusoutput('javac %s' % path.join('.', 'TestContainerAndBall.java'))
-    if code is not 0: 
-        print('test file can not be compiled')
-        print(output)
+    code, output = subprocess.getstatusoutput('java %s' % name)
+    if code is not 0:
+        print('%s failed' % name)
         return 0
-    for file in filelist:
-        code, output = subprocess.getstatusoutput('javac %s' % path.join('.', file))
-        if code is not 0:
-            print('user file can not be compiled')
-            return 0
-    code, output = subprocess.getstatusoutput('java TestContainerAndBall')
-    return int(output)
+    return int(output.split('\n')[-1])
 
 workspace = path.join('.', 'user')
 filelist = ['Ball.java', 'Container.java', 'DateUtil.java', 'Line.java', 'Point.java']
@@ -70,20 +30,11 @@ for file in files:
     if file not in filelist:
         remove(path.join(workspace, file))
 
-task1 = testDateUtil()
-task2 = testPointAndLine()
-task3 = testContainerAndBall()
-print(task1, task2, task3)
+task1 = test('TestDateUtil', ['DateUtil.java'])
+task2 = test('TestPointAndLine', ['Point.java', 'Line.java'])
+task3 = test('TestContainerAndBall', ['Container.java', 'Ball.java'])
+print(task1, task2, task3, '==>', task1 * 0.3 + task2 * 0.3 + task3 * 0.4)
 subprocess.getoutput('rm *.class *.java')
-# print(path.join('.', filelist[0]))
 
-# code, output = subprocess.getstatusoutput('javac TestDateUtil.java')
-# print(code)
-# print(output.split('\n'))
-
-# files = listdir(workspace)
-# for file in files:
-    # remove(path.join(workspace, file))
-
-
-
+for file in filelist:
+    remove(path.join(workspace, file))
