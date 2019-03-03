91e4e3dba60ebfbb6ab4d41031b0c149fb3e7c57
Wycer <734849165@qq.com>
2019-03-03 17:24:14 +0800

    lab1 2

---
+++ CS202/lab1/ex3.asm
@@ -0,0 +1,25 @@
+# ===========================================
+# Calculate (a + b * c) / d
+# Author: 11711918 wyc
+# ===========================================
+
+.data
+	str:	.asciiz	"abc"
+.text
+	la		$t1, str
+	
+	lb		$t0, 0($t1)
+	subu	$t2, $t0, 32
+	sb		$t2, 0($t1)
+
+	lb		$t0, 1($t1)
+	subu	$t2, $t0, 32
+	sb		$t2, 1($t1)
+	
+	lb		$t0, 2($t1)
+	subu	$t2, $t0, 32
+	sb		$t2, 2($t1)
+	
+	la		$a0, str
+	li		$v0, 4
+	syscall
