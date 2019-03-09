010124193560f8a15c8ce96296a7210bb6a949b2
Wycer <734849165@qq.com>
2019-03-09 16:18:10 +0800

    modify

--- CS208/exercise1/main.py
+++ CS208/exercise1/main.py
@@ -8,15 +8,16 @@ def cal(i, f):
     end = time.process_time()
     return end - start
 
+cases = [3 ** i for i in range(0, 8)]
 f = open('res.csv', 'w')
 f.write('complexity')
-for i in [3 ** i for i in range(0, 8)]:
+for i in cases:
     f.write(', n = %d' % i)
 f.write('\n')
 
 for now in [n, nlogn, n2, n3, nf]:
     f.write('O(%s)' % now.__name__)
-    for i in [3 ** i for i in range(0, 8)]:
+    for i in cases:
         f.write(', %f' % cal(i, now.work))
     f.write('\n')
 close(f)
