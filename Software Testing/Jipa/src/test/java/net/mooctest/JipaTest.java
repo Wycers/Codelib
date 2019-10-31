cefb86a1eebacba24170183d640c1fa89fe3c75f
wycers <wycers@gmail.com>
2019-10-31 13:16:45 +0800

    st two practice

---
+++ Software Testing/Jipa/src/test/java/net/mooctest/JipaTest.java
@@ -0,0 +1,17 @@
+package net.mooctest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class JipaTest {
+
+  @Test(timeout = 4000)
+  public void test() {
+	  int int0 = Jipa.getValue("");
+  }
+
+}
