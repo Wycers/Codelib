bf2f9974a165c9edb4ad913466b162b994e214ed
Wycer <wycers@gmail.com>
2019-11-06 00:17:58 +0800

    cs301 lab8

---
+++ CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/DependInfo.cmake
@@ -0,0 +1,77 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/wycer/Codelib/CS301/lab8/startup/startup_stm32f103xe.s" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/startup/startup_stm32f103xe.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xE"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Src/gpio.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Src/gpio.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Src/main.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Src/main.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Src/stm32f1xx_hal_msp.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Src/stm32f1xx_it.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Src/stm32f1xx_it.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Src/sys.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Src/sys.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Src/syscalls.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Src/syscalls.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Src/system_stm32f1xx.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Src/system_stm32f1xx.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Src/tim.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Src/tim.c.obj"
+  "/Users/wycer/Codelib/CS301/lab8/Src/usart.c" "/Users/wycer/Codelib/CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/Src/usart.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F103xE"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CS301/lab8/cmake-build-debug/CMakeFiles/test.elf.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  "CMakeFiles/test.elf.dir/Src/gpio.c.obj"
+  "CMakeFiles/test.elf.dir/Src/main.c.obj"
+  "CMakeFiles/test.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/test.elf.dir/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/test.elf.dir/Src/sys.c.obj"
+  "CMakeFiles/test.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/test.elf.dir/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/test.elf.dir/Src/tim.c.obj"
+  "CMakeFiles/test.elf.dir/Src/usart.c.obj"
+  "CMakeFiles/test.elf.dir/startup/startup_stm32f103xe.s.obj"
+  "test.elf"
+  "test.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/test.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
