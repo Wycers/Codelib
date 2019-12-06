92d4faaba0f1e6615c798965cac84aa7a2b83b98
Wycer <wycers@gmail.com>
2019-12-06 15:39:22 +0800

    I cannot

--- CS309/assignment4/diassignment/src/main/java/cn/edu/sustech/ContainerImpl.java
+++ CS309/assignment4/diassignment/src/main/java/cn/edu/sustech/ContainerImpl.java
@@ -3,16 +3,25 @@ package cn.edu.sustech;
 public class ContainerImpl implements Container {
     @Override
     public <T> void register(Class<T> serviceType) {
+        if (serviceType == null) {
+            throw new IllegalArgumentException();
+        }
 
     }
 
     @Override
     public <T> void register(Class<T> serviceType, Class<? extends T> implementationType) {
+        if (serviceType == null || implementationType == null) {
+            throw new IllegalArgumentException();
+        }
 
     }
 
     @Override
     public <T> T resolve(Class<T> serviceType) {
+        if (serviceType == null) {
+            throw new IllegalArgumentException();
+        }
         return null;
     }
 }
