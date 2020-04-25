7d3fa6afd3811dfd2164fabfe4ed23ccdb16db38
wycers <wycers@gmail.com>
2020-04-25 14:18:45 +0800

    cs302 lab6

--- CS302/lab6/code/main.cpp
+++ CS302/lab6/code/main.cpp
@@ -3,21 +3,21 @@
 
 using namespace std;
 
-#define PROCESS_NAME_LEN 32         // 进程名最大长度
-#define MIN_SLICE 10                // 内碎片最大大小
-#define DEFAULT_MEM_SIZE 1024       // 总内存大小
-#define DEFAULT_MEM_START 0         // 内存开始分配时的起始地址
+#define PROCESS_NAME_LEN 32   // 进程名最大长度
+#define MIN_SLICE 10          // 内碎片最大大小
+#define DEFAULT_MEM_SIZE 1024 // 总内存大小
+#define DEFAULT_MEM_START 0   // 内存开始分配时的起始地址
 
 int mem_size = DEFAULT_MEM_SIZE;
-static int pid = 0;
+static int global_pid = 0;
 
-struct free_block {                 // 空闲数据块
+struct free_block { // 空闲数据块
     int size;
     int start_addr;
     struct free_block *next;
 };
 
-struct allocated_block {            // 已分配的数据块
+struct allocated_block { // 已分配的数据块
     int pid;
     int size;
     int start_addr;
@@ -25,31 +25,29 @@ struct allocated_block {            // 已分配的数据块
     allocated_block *next;
 };
 
-free_block *free_block_head;        // 空闲数据块首指针
-allocated_block *allocated_block_head = nullptr;    // 分配块首指针
+free_block *free_block_head;                     // 空闲数据块首指针
+allocated_block *allocated_block_head = nullptr; // 分配块首指针
 
+allocated_block *find_process(int id); // 寻找pid为id的分配块
+free_block *init_free_block(int size); // 空闲块初始化
+void display_menu();                   //显示选项菜单
+void set_mem_size(int arg);            // 设置内存大小
 
-
-allocated_block *find_process(int id);              // 寻找pid为id的分配块
-free_block *init_free_block(int mem_size);          // 空闲块初始化
-void display_menu(); //显示选项菜单
-void set_mem_size(int arg); // 设置内存大小
-
-void rearrange(); // 对块进行重新分配
-void swap(int *p, int *q); //交换地址
+void rearrange();                 // 对块进行重新分配
+void swap(int *p, int *q);        //交换地址
 int dispose(allocated_block *ab); //释放分配块结构体
-void display_mem_usage(); //显示内存情况
-int create_new_process(int mem_size);               //创建新的进程
-void kill_process(int pid); //杀死对应进程并释放其空间与结构体
+void display_mem_usage();         //显示内存情况
+int create_new_process(int size); //创建新的进程
+void kill_process(int pid);       //杀死对应进程并释放其空间与结构体
 
 class Allocator {
 public:
     virtual int free_mem(allocated_block *ab);
-    virtual int allocate_mem(allocated_block *ab) = 0;  //为制定块分配内存
+
+    virtual int allocate_mem(allocated_block *ab) = 0; //为制定块分配内存
 };
 
-
-void insert_allocate_block(allocated_block* target) {
+void insert_allocate_block(allocated_block *target) {
     if (allocated_block_head == nullptr) {
         target->next = nullptr;
         allocated_block_head = target;
@@ -58,7 +56,8 @@ void insert_allocate_block(allocated_block* target) {
         allocated_block_head = target;
     }
 }
-void remove_allocate_block(allocated_block* target) {
+
+void remove_allocate_block(allocated_block *target) {
     if (allocated_block_head == target) {
         allocated_block_head = allocated_block_head->next;
     } else {
@@ -71,7 +70,8 @@ void remove_allocate_block(allocated_block* target) {
     }
     delete target;
 }
-void insert_free_block(free_block* target) {
+
+void insert_free_block(free_block *target) {
     if (free_block_head == nullptr) {
         free_block_head = target;
     } else {
@@ -79,7 +79,8 @@ void insert_free_block(free_block* target) {
         free_block_head = target;
     }
 }
-void remove_free_block(free_block* target) {
+
+void remove_free_block(free_block *target) {
     if (free_block_head == target) {
         free_block_head = free_block_head->next;
     } else {
@@ -100,6 +101,7 @@ free_block *find_back_free_block(int start_addr) {
     }
     return nullptr;
 }
+
 free_block *find_front_free_block(int end_addr) {
     for (free_block *now = free_block_head; now != nullptr; now = now->next) {
         if (now->start_addr + now->size == end_addr)
@@ -108,11 +110,11 @@ free_block *find_front_free_block(int end_addr) {
     return nullptr;
 }
 
-int Allocator::free_mem(allocated_block *ab) {         // 释放分配块
+int Allocator::free_mem(allocated_block *ab) { // 释放分配块
     // 1. 查找前部有无空闲分区
-    free_block* front = find_front_free_block(ab->start_addr);
+    free_block *front = find_front_free_block(ab->start_addr);
     // 2. 查找后部有无空闲分区
-    free_block* back = find_back_free_block(ab->start_addr + ab->size);
+    free_block *back = find_back_free_block(ab->start_addr + ab->size);
     if (front != nullptr) {
         front->size += ab->size;
         if (back != nullptr) {
@@ -127,7 +129,7 @@ int Allocator::free_mem(allocated_block *ab) {         // 释放分配块
         return 1;
     }
     // 开辟一个空内存
-    free_block *newFree = new free_block();
+    auto *newFree = new free_block();
     newFree->size = ab->size;
     newFree->start_addr = ab->start_addr;
     newFree->next = nullptr;
@@ -141,10 +143,10 @@ class FirstFitAllocator : public Allocator {
         int need = ab->size;
         free_block *ptr = nullptr;
         for (free_block *now = free_block_head; now != nullptr; now = now->next) {
-            if (now->size >= need) {
-                ptr = now;
-                break;
-            }
+            if (now->size < need)
+                continue;
+            ptr = now;
+            break;
         }
         if (ptr == nullptr) {
             return -1;
@@ -164,26 +166,67 @@ class FirstFitAllocator : public Allocator {
 
 class WorstFitAllocator : public Allocator {
     int allocate_mem(allocated_block *ab) override {
+        int need = ab->size;
+        free_block *ptr = nullptr;
+        // find the largest free block
+        for (free_block *now = free_block_head; now != nullptr; now = now->next) {
+            if (now->size < need)
+                continue;
+            if (ptr == nullptr || now->size > ptr->size) {
+                ptr = now;
+            }
+        }
+        if (ptr == nullptr) {
+            return -1;
+        }
 
+        if (ptr->size == need) {
+            remove_free_block(ptr);
+        } else {
+            ptr->size -= need;
+            ab->start_addr = ptr->start_addr;
+            ptr->start_addr += need;
+        }
+        insert_allocate_block(ab);
+        return 0;
     }
 };
 
 class BestFitAllocator : public Allocator {
     int allocate_mem(allocated_block *ab) override {
+        int need = ab->size, mn = 0x7fffffff;
+        free_block *ptr = nullptr;
+        // find the smallest but large enough free block
+        for (free_block *now = free_block_head; now != nullptr; now = now->next) {
+            if (now->size < need)
+                continue;
+            if (ptr == nullptr || ptr->size > now->size) {
+                ptr = now;
+            }
+        }
+        if (ptr == nullptr) {
+            return -1;
+        }
 
+        if (ptr->size == need) {
+            remove_free_block(ptr);
+        } else {
+            ptr->size -= need;
+            ab->start_addr = ptr->start_addr;
+            ptr->start_addr += need;
+        }
+        insert_allocate_block(ab);
+        return 0;
     }
 };
 
 class BuddySystemAllocator : public Allocator {
     int free_mem(allocated_block *ab) override {
-
     }
 
     int allocate_mem(allocated_block *ab) override {
-
     }
 };
-
 
 Allocator *alc;
 
@@ -226,10 +269,9 @@ int main() {
     }
 }
 
-
-free_block *init_free_block(int mem_size) { //初始化空闲块，这里的mem_size表示允许的最大虚拟内存大小
-    free_block *p = new free_block;
-    p->size = mem_size;
+free_block *init_free_block(int size) { //初始化空闲块，这里的mem_size表示允许的最大虚拟内存大小
+    auto *p = new free_block;
+    p->size = size;
     p->start_addr = DEFAULT_MEM_START;
     p->next = nullptr;
     return p;
@@ -257,17 +299,17 @@ int allocate_mem(allocated_block *ab) { //为块分配内存，真正的操作�
     return alc->allocate_mem(ab);
 }
 
-int create_new_process(int mem_size) { //创建新进程
+int create_new_process(int size) { //创建新进程
     if (alc == nullptr) {
         puts("Set memory allocation algorithm first!");
         return -1;
     }
 
     // Write your code here
-    allocated_block *newBlock = new allocated_block();
+    auto *newBlock = new allocated_block();
 
-    newBlock->pid = ++pid;
-    newBlock->size = mem_size;
+    newBlock->pid = ++global_pid;
+    newBlock->size = size;
     newBlock->next = nullptr;
 
     allocate_mem(newBlock);
@@ -299,7 +341,6 @@ void rearrange() { //将块按照地址大小进行排序
     puts("Rearrange Done.");
 }
 
-
 int free_mem(allocated_block *ab) { //释放某一块的内存
     return alc->free_mem(ab);
 }
@@ -319,8 +360,10 @@ void display_mem_usage() {
         printf("%20d %20d\n", fb->start_addr, fb->size);
         fb = fb->next;
     }
-    if (!cnt) puts("No Free Memory");
-    else printf("Totaly %d free blocks\n", cnt);
+    if (!cnt)
+        puts("No Free Memory");
+    else
+        printf("Totaly %d free blocks\n", cnt);
     puts("");
     puts("*******************Used Memory*********************");
     printf("%10s %10s %20s\n", "PID", "start_addr", "size");
@@ -330,10 +373,11 @@ void display_mem_usage() {
         printf("%10d %10d %20d\n", ab->pid, ab->start_addr, ab->size);
         ab = ab->next;
     }
-    if (!cnt) puts("No allocated block");
-    else printf("Totaly %d allocated blocks\n", cnt);
+    if (!cnt)
+        puts("No allocated block");
+    else
+        printf("Totaly %d allocated blocks\n", cnt);
 }
-
 
 allocated_block *find_process(int id) { //循环遍历分配块链表，寻找pid=id的进程所对应的块
     for (allocated_block *now = allocated_block_head; now != nullptr; now = now->next) {
@@ -355,6 +399,3 @@ void kill_process(int pid) { //杀死某个进程
         dispose(ab);
     }
 }
-
-
-
