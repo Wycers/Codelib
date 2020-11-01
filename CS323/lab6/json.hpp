28c51d961ab0cc7610f7b1bd700af63065db9df8
Wycer <wycers@gmail.com>
2020-11-01 21:17:40 +0800

    cs323 lab6

--- CS323/lab6/json.hpp
+++ CS323/lab6/json.hpp
@@ -1,45 +1,66 @@
 #include <cstring>
+
 using namespace std;
 
 
+typedef struct BOOLEAN {
+    bool val;
+} BOOLEAN;
 
-typedef struct Member {
-
-};
-
-typedef struct Members {
-
-};
-
-
-typedef struct Object {
-
-};
-typedef struct Array {
-
-};
 typedef struct STRING {
+    char *str;
+} STRING;
 
-};
 typedef struct NUMBER {
-    string str;
-
-};
-
-typedef struct Values {
-
-};
+    int val;
+} NUMBER;
 
 typedef struct Value {
     char type[32];
-    enum {Object, Array, STRING, NUMBER, TRUE, FALSE, VNULL} category;
+    enum {
+        Object, Array, STRING, NUMBER, BOOLEAN, VNULL
+    } category;
     union {
         struct Object *object;
         struct Array *array;
         struct STRING *string;
+        struct NUMBER *number;
+        struct BOOLEAN *boolean;
     };
-};
+} Value;
 
-typedef struct Json {
-    struct Value* value;
-};
+typedef struct Values {
+    struct Value *value;
+    struct Values *next;
+} Values;
+
+
+typedef struct Member {
+    char *key;
+    struct Value *value;
+} Member;
+
+typedef struct Members {
+    struct Member *member;
+    struct Members *next;
+} Members;
+
+
+typedef struct Object {
+    struct Members *members;
+} Object;
+
+typedef struct Array {
+    struct Values *values;
+} Array;
+
+typedef struct JSON {
+    enum {
+        Object, Array
+    } category;
+
+    union {
+        struct Object *object;
+        struct Array *array;
+    };
+} JSON;
